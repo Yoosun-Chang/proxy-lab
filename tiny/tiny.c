b7e9bb5a12b79204d969b396df9028a72a447da5
Yoosun-Chang <12201321@inha.edu>
2024-04-19 10:44:05 +0000

    Comment: main 함수 주석 추가(#1)

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -18,26 +18,28 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
                  char *longmsg);
 
 int main(int argc, char **argv) {
-  int listenfd, connfd;
-  char hostname[MAXLINE], port[MAXLINE];
-  socklen_t clientlen;
-  struct sockaddr_storage clientaddr;
+  int listenfd, connfd; // 서버 및 클라이언트 소켓 파일 디스크립터
+  char hostname[MAXLINE], port[MAXLINE]; // 클라이언트 호스트네임 및 포트번호
+  socklen_t clientlen; // 클라이언트 주소 구조체 크기
+  struct sockaddr_storage clientaddr; // 클라이언트 주소 구조체
 
-  /* Check command line args */
-  if (argc != 2) {
+  // 명령행 인수 확인
+  if (argc != 2) { // 인수 개수가 2가 아니면 오류 메시지 출력
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
 
-  listenfd = Open_listenfd(argv[1]);
+  // 클라이언트 연결 수신 소켓 생성
+  listenfd = Open_listenfd(argv[1]); 
+
+  // 클라이언트 요청 수락 및 처리
   while (1) {
-    clientlen = sizeof(clientaddr);
-    connfd = Accept(listenfd, (SA *)&clientaddr,
-                    &clientlen);  // line:netp:tiny:accept
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
-                0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    doit(connfd);   // line:netp:tiny:doit
-    Close(connfd);  // line:netp:tiny:close
+    clientlen = sizeof(clientaddr); // 클라이언트 주소 구조체 크기 설정
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 연결 수락
+    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0); // 클라이언트 호스트네임 및 포트번호 추출
+    printf("Accepted connection from (%s, %s)\n", hostname, port); // 연결 확인 메시지 출력
+    doit(connfd); // 클라이언트 요청 처리 함수 호출
+    Close(connfd); // 연결 종료 및 소켓 닫음
   }
 }
+
